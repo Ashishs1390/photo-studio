4d6cc77abe3ae64dabe0c4ddab98fed283f86a85
Ashishs1390 <ashishhsharma1990@gmail.com>
2020-04-11 18:30:02 +0530

    package design

--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -6,7 +6,7 @@ function Packages(){
     return(
        <Container fluid={true}>
            <Row>
-                <Col md="6" className="PackageText">
+                <Col md="6" className="PackageText Section-1">
                     <h3 className="PackageHeader">
                         Service Offered
                     </h3>
@@ -26,9 +26,43 @@ function Packages(){
                 </Col>
                 <Col md="6" className="PackageImg">
                   <div className="ImgSection">
-
                   </div>
                 </Col>
+           </Row>
+           <Row>
+               <Col md="6">
+               <div className="ImgSection">
+                  </div>
+               </Col>
+               <Col md="6" className="PackageText Section-2">
+                   <h3 className="PackageHeader"> 
+                       Foundation Course
+                   </h3>
+                   <p>The foundation course provides basic training in professional makeup skill and artistry covering day, night,
+                       corporate and makeup for special occasions. The students will be trained in.
+                   </p>
+                   <ul className="PackageLists">
+                       <li className="PackageList">
+                            Everday Makeup
+                       </li>
+                       <li className="PackageList">Day and Night makeup</li>
+                       <li className="PackageList">Corporate makeup</li>
+                       <li className="PackageList">Party makeup</li>
+                       <li className="PackageList">Occasion makeup</li>
+                       <li className="PackageList">Engagement makeup</li>
+                   </ul>
+               </Col>
+           </Row>
+           <Row>
+               <Col md="6" className="PackageText Section-3">
+               <h3 className="PackageHeader"> 
+                       Professional Makeup Artist Course
+                   </h3>
+               </Col>
+               <Col md="6">
+               <div className="ImgSection">
+                  </div>
+               </Col>
            </Row>
            
        </Container>
