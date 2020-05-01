a46adc036f920f4bd77bbb8e3ecd2799a8807290
Ashishs1390 <ashishhsharma1990@gmail.com>
2020-05-02 00:46:56 +0530

    with light box

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useState,useEffect,useCallback} from 'react';
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 function Portfolio(){
      const photos = [
@@ -89,13 +90,42 @@ function Portfolio(){
           height: 3
         }
       ];
+
+      const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
       
       
 
 
     return(
         <div>
-          <Gallery photos={photos} />
+          <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
 
         </div>
     )
