8d8513082bc0564db9afe7c699b9193acef31d17
Ashishs1390 <ashishhsharma1990@gmail.com>
2020-05-02 14:54:00 +0530

    portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,132 +1,149 @@
 import React,{useState,useEffect,useCallback} from 'react';
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
+import './Portfolio.scss';
+
+import row10 from './../../images/portfolio/img.jpg'
+import row11 from './../../images/portfolio/img1.jpg'
+import row12 from './../../images/portfolio/img2.jpg'
+import row13 from './../../images/portfolio/img3.jpg'
+import row14 from './../../images/portfolio/img4.jpg'
+import row15 from './../../images/portfolio/img5.jpg'
+import row20 from './../../images/portfolio/img17.jpg'
+import row21 from './../../images/portfolio/img7.jpg'
+import row22 from './../../images/portfolio/img8.jpg'
+import row23 from './../../images/portfolio/img9.jpg'
+import row24 from './../../images/portfolio/img10.jpg'
+import row31 from './../../images/portfolio/img11.jpg'
+import row32 from './../../images/portfolio/img12.jpg'
+import row40 from './../../images/portfolio/img13.jpg'
+import row41 from './../../images/portfolio/img14.jpg'
+import row42 from './../../images/portfolio/img15.jpg'
+import row43 from './../../images/portfolio/img16.jpg'
+
 
 function Portfolio(){
      const photos = [
         {
-          src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-          width: 4,
-          height: 3
+          src: row10,
+          width: 3,
+          height: 4
         },
         {
-          src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+          src: row11,
           width: 1,
           height: 1
         },
         {
-          src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+          src: row12,
+          width: 5,
+          height: 4
+        },
+        {
+          src: row13,
+          width: 4,
+          height: 4
+        },
+        {
+          src: row14,
           width: 3,
           height: 4
         },
         {
-          src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+          src: row15,
+          width: 4,
+          height: 3
+        },
+        {
+          src: row20,
           width: 3,
           height: 4
         },
         {
-          src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+          src: row21,
+          width: 4,
+          height: 3
+        },
+        {
+          src: row22,
+          width: 4,
+          height: 3
+        },
+        {
+          src: row23,
           width: 3,
           height: 4
         },
         {
-          src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+          src: row24,
           width: 4,
           height: 3
         },
         {
-          src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-          width: 3,
-          height: 4
+          src: row31,
+          width: 9,
+          height: 5
         },
         {
-          src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+          src: row32,
           width: 4,
           height: 3
         },
         {
-          src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+          src: row40,
           width: 4,
           height: 3
         },
         {
-          src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+          src: row41,
           width: 4,
           height: 3
         },
         {
-          src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
-          width: 4927,
-          height: 1000
-        },
-        {
-          src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
+          src: row42,
           width: 4,
           height: 3
         },
         {
-          src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
+          src: row43,
           width: 4,
           height: 3
         }
       ];
 
-      const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
 
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
       
       
 
 
     return(
-        <div>
-          <div>
-      <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
-    </div>
-
+        <div className="Portfolio">
+        <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                    <Carousel
+                    currentIndex={currentImage}
+                    views={photos.map(x => ({
+                        ...x,
+                        srcset: x.srcSet,
+                        caption: x.title
+                    }))}
+                    />
+                </Modal>
+                ) : null}
+            </ModalGateway>
         </div>
     )
 }
